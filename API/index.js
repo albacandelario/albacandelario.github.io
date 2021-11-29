6c064e6f726e78c9f8ed6d16ac9f2cc10b79b8fa
loridev <79363993+loridev@users.noreply.github.com>
2021-11-29 17:00:44 +0100

    Nuevas rutas

--- API/index.js
+++ API/index.js
@@ -7,6 +7,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/api/v1/ranking/', require('./routes/rankingsRoutes'));
-app.use('/api/v1/items/', require('./routes/itemsRoutes'));
-app.use('/api/v1/shop', require('./routes/shopRoutes'));
+app.use('/v1/ranking/', require('./routes/rankingsRoutes'));
+app.use('/v1/items/', require('./routes/itemsRoutes'));
+app.use('/v1/shop', require('./routes/shopRoutes'));
